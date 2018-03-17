b571f9f9a88b777b797f98ff6976d4e71542738b
Mohammadreza Deljavan <31951305+mdeljavan@users.noreply.github.com>
2018-03-18 01:27:54 +0430

    fix bug in and lib date work perfectly

--- src/lib/changeDate.js
+++ src/lib/changeDate.js
@@ -6,10 +6,10 @@ License: GNU/LGPL _ Open Source & Free _ Version: 2.72 : [2017=1396]
 12053 = 365*33 + 32/4    &    36524 = 365*100 + 100/4 - 100/100   */
 
 
-const gregorian_to_jalali = (gy, gm, gd) => {
-    const g_d_m = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+const gregorian_to_jalali = ( gy, gm, gd ) => {
+    const g_d_m = [ 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334 ];
     let jy;
-    if (gy > 1600) {
+    if ( gy > 1600 ) {
         jy = 979;
         gy -= 1600;
     } else {
@@ -19,26 +19,26 @@ const gregorian_to_jalali = (gy, gm, gd) => {
     gd = +gd;
     gy = +gy;
     gm = +gm;
-    const gy2 = +(((gm > 2) ? (gy + 1) : gy));
-    let days = (365 * gy) + ((gy2 + 3) / 4) - ((gy2 + 99) / 100) + ((gy2 + 399) / 400) - 80 + gd + g_d_m[gm - 1];
-    jy += 33 * (parseInt(days / 12053));
+    const gy2 = ( ( gm > 2 ) ? ( gy + 1 ) : gy );
+    let days = ( 365 * gy ) + Math.floor( ( gy2 + 3 ) / 4 ) - Math.floor( ( gy2 + 99 ) / 100 ) + Math.floor( ( gy2 + 399 ) / 400 ) - 80 + gd + g_d_m[ gm - 1 ];
+    jy += 33 * ( Math.floor( days / 12053 ) );
     days %= 12053;
-    jy += 4 * (parseInt(days / 1461));
+    jy += 4 * ( Math.floor( days / 1461 ) );
     days %= 1461;
-    if (days > 365) {
-        jy += (days - 1) / 365;
-        days = (days - 1) % 365;
+    if ( days > 365 ) {
+        jy += Math.floor( ( days - 1 ) / 365 );
+        days = ( days - 1 ) % 365;
     }
-    const jm = (days < 186) ? 1 + days / 31 : 7 + (days - 186) / 30;
-    const jd = 1 + ((days < 186) ? (days % 31) : ((days - 186) % 30));
-    return [Math.floor(jy), Math.floor(jm), Math.floor(jd)];
+    const jm = ( days < 186 ) ? Math.floor( 1 + days / 31 ) : Math.floor( 7 + ( days - 186 ) / 30 );
+    const jd = 1 + ( ( days < 186 ) ? ( days % 31 ) : ( ( days - 186 ) % 30 ) );
+    return [ jy, jm, jd ];
 }
 
-console.log(gregorian_to_jalali(2018, 3, 17))
+console.log( gregorian_to_jalali( 2018, 3, 18 ) )
 
-const jalali_to_gregorian = (jy, jm, jd) => {
+const jalali_to_gregorian = ( jy, jm, jd ) => {
     let gy;
-    if (jy > 979) {
+    if ( jy > 979 ) {
         gy = 1600;
         jy -= 979;
     } else {
@@ -47,26 +47,26 @@ const jalali_to_gregorian = (jy, jm, jd) => {
     jy = +jy;
     jm = +jm;
     jd = +jd;
-    let days = (365 * jy) + ((jy / 33) * 8) + (((jy % 33) + 3) / 4) + 78 + jd + ((jm < 7) ? (jm - 1) * 31 : ((jm - 7) * 30) + 186);
-    gy += 400 * (parseInt(days / 146097));
+    let days = ( 365 * jy ) + Math.floor( jy / 33 ) * 8 + Math.floor( ( ( jy % 33 ) + 3 ) / 4 ) + 78 + jd + ( ( jm < 7 ) ? ( jm - 1 ) * 31 : ( ( jm - 7 ) * 30 ) + 186 );
+    gy += 400 * ( Math.floor( days / 146097 ) );
     days %= 146097;
-    if (days > 36524) {
-        gy += 100 * (parseInt(--days / 36524));
+    if ( days > 36524 ) {
+        gy += 100 * ( Math.floor( --days / 36524 ) );
         days %= 36524;
-        if (days >= 365) days++;
+        if ( days >= 365 ) days++;
     }
-    gy += 4 * (parseInt(days / 1461));
+    gy += 4 * ( Math.floor( days / 1461 ) );
     days %= 1461;
-    if (days > 365) {
-        gy += parseInt((days - 1) / 365);
-        days = (days - 1) % 365;
+    if ( days > 365 ) {
+        gy += Math.floor( ( days - 1 ) / 365 );
+        days = ( days - 1 ) % 365;
     }
     let gd = days + 1;
-    const sal_a = [0, 31, ((gy % 4 == 0 && gy % 100 != 0) || (gy % 400 == 0)) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    for (var gm = 0; gm < 13; gm++) {
-        const v = sal_a[gm];
-        if (gd <= v) break;
+    const sal_a = [ 0, 31, ( ( gy % 4 == 0 && gy % 100 != 0 ) || ( gy % 400 == 0 ) ) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+    for ( var gm = 0; gm < 13; gm++ ) {
+        const v = sal_a[ gm ];
+        if ( gd <= v ) break;
         gd -= v;
     }
-    return [Math.floor(gy), Math.floor(gm), Math.floor(gd)];
+    return [ gy, gm, gd ];
 }
